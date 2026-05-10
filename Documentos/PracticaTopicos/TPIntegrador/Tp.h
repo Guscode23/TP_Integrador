2139b9a26f6ccc750389963e2a0483780c4dbd3e
Pablitosilva2021 <89499068+Pablitosilva2021@users.noreply.github.com>
2026-05-10 20:44:08 -0300

    Update Tp.h

--- Documentos/PracticaTopicos/TPIntegrador/Tp.h
+++ Documentos/PracticaTopicos/TPIntegrador/Tp.h
@@ -1,6 +1,30 @@
 #ifndef TP_H_INCLUDED
 #define TP_H_INCLUDED
 
+#include <stdio.h>
+#include <stdlib.h>
 
+#define TAM_APE_NOM 60
+#define TAM_CAT 10
+#define TAM_PLAN 10
+#define TAM_EMAIL_TUT 30
+
+//TDA FECHA
+typedef struct{
+    int dia, mes, anio;
+}t_fecha;
+
+typedef struct{
+    long dni;
+    char apeNom[TAM_APE_NOM];
+    t_fecha fechNac;
+    char sexo;
+    t_fecha fechAfil;
+    char cat[TAM_CAT];
+    t_fecha fechUltCuot;
+    char estado;
+    char plan[TAM_PLAN];
+    char emailTutor[TAM_EMAIL_TUT];
+}t_datos;
 
 #endif // TP_H_INCLUDED
