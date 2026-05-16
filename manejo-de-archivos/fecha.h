ccbe69656c79ba8fca3289efff83858256da0bef
facugabriel411 <facugabriel411@gmail.com>
2026-05-16 18:59:53 -0300

    Incorporacion TDA FECHA

---
+++ manejo-de-archivos/fecha.h
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#define TODO_OK 1
+#define ERROR 0
+
+///TDA
+typedef struct{
+    int dia, mes, anio;
+}t_fecha;
+
+///MAIN
+void ingresar_Fecha_Proceso(t_fecha *f);
+
+///VALIDACION DE FECHA
+int es_Fecha_Valida(const t_fecha *f);
+int cant_Dia_Mes (int mes, int anio);
+bool es_Bisiesto(int anio);
+
+
