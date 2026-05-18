c106f6693cade7918aa3290f6d9fca1ef8c6bc17
Gustavo Rosales <rosalesunlam19@gmail.com>
2026-05-17 23:10:10 -0300

    Agregado de funciones de validación de fechas dentro de la biblioteca

--- Documentos/PracticaTopicos/TPIntegrador/Validaciones_Cinefilia/fecha.h
+++ Documentos/PracticaTopicos/TPIntegrador/Validaciones_Cinefilia/fecha.h
@@ -4,6 +4,8 @@
 
 #define TODO_OK 1
 #define ERROR 0
+#define FALLA_DATO 1
+#define DATO_OK 0
 
 ///TDA
 typedef struct{
@@ -17,5 +19,8 @@ void ingresar_Fecha_Proceso(t_fecha *f);
 int es_Fecha_Valida(const t_fecha *f);
 int cant_Dia_Mes (int mes, int anio);
 bool es_Bisiesto(int anio);
+///ULTIMA CUOTA PAGA
+int validar_UltimaCuota_Paga(t_fecha* fechAfi, t_fecha* fechUltCuot, t_fecha* fProceso);
+int compara_Fechas_Meno_rIgual(t_fecha *fechA, t_fecha *fechB); /// Esta tmbien se podria usar en validar afilacion
 
 
